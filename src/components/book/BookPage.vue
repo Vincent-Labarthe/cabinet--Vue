3598ec41f595e6ab31698ccb1e96ba72842ce115
Vincent <v.labarthe@gmail.com>
2020-10-13 13:42:05 +0200

    author+video

--- src/components/book/BookPage.vue
+++ src/components/book/BookPage.vue
@@ -72,43 +72,74 @@
           corps et permettre l’aisance gestuelle et la progression.
         </p>
         <div class="profile-picture--box">
-          <img class="profile-picture" src="../../../public/img/josephine-lyon.jpg" alt="">
+          <div class="profile-box profile-box--1">
+            <img class="profile-picture" src="../../../public/img/josephine-lyon.jpg" alt="">
+          </div>
         </div>
       </div>
-
     </div>
     <div class="author__box author__box--2">
       <h3 class="author__box author__box--header header--secondary author__box--header--2"> Frédérique Brigaud</h3>
       <div class="author__box author__box--modal">
         <div class="profile-picture--box">
-          <img class="profile-picture" src="../../../public/img/fred-brigaud.jpg" alt="">
+          <div class="profile-box profile-box--2">
+            <img class="profile-picture" src="../../../public/img/fred-brigaud.jpg" alt="">
+          </div>
         </div>
-        <p class="author__text">Ostéopathe DO exerçant à Paris depuis 2011, est passionnée d’équitation
-          depuis l’enfance.
-          Cavalière de compétition depuis 1999, niveau club puis amateur (nombreux podiums par équipe au championnat de
-          France CSO et CCE), elle expérimente différentes disciplines : le hunter, l’obstacle, le dressage et surtout
-          le concours complet qu’elle affectionne particulièrement. <br><br>
-          Elle choisit de mêler son métier et sa passion en devenant ostéopathe du sport et en se formant en ostéopathie
-          équine à l'IFOREC. Elle prend conscience des énormes lacunes dans la préparation physique des cavaliers en
-          comparaison aux autres sports. Elle identifie les risques qu’ils encourent en pratiquant de la sorte et
-          réalise
-          l’impact d’une pratique inadaptée sur la performance du couple cavalier-cheval mais aussi sur leur santé
-          respective. Elle s’interroge sur le moyen d’améliorer la condition physique du cavalier et comment intégrer le
-          confort du cheval à cette discipline. Dans l’écriture comme dans sa pratique sportive, pourtant habituellement
-          individuelle, elle privilégie le « travail » d’équipe. Elle contacte Frédéric Brigaud, spécialiste en
-          biomécanique du sport dont les concepts ead, visant à donner des solutions pérennes au sportif plutôt que de
-          le
-          soigner périodiquement, lui semblent être la solution. Ainsi a été co-écrit l’ouvrage Repenser l’équitation,
-          fruit de six années de réflexion, à travers ces différents points de vue. Des réponses simples à mettre en
-          pratique sont proposées à tous les acteurs de la vie équestre, pour constituer de nouvelles bases, préserver
-          son
-          corps et permettre l’aisance gestuelle et la progression.
+        <p class="author__text">Consultant en biomécanique Humaine, Ostéopathe DO auprès de sportifs de haut niveau depuis 1994 :
+          <br><br>
+          - Consultant en biomécanique Humaine et sportive, Ostéopathe.DO de formation auprès de sportifs de haut niveau depuis 1994 : Trail, football, Ski alpin, Ski de fond, biathlon…
+
+          - Concepteur des principes posturo-dynamiques d’Empilement Articulaire Dynamique, pour l'organisation et la gestion du corps et des stratégies biomécaniques dans le mouvement, enseignés en Kinésithérapie du sport et auprès des BE Sport (Ski alpin, Tennis, Golf, Surf…)
+
+          - Consultant et formateur sport/santé pour UCPA Loisir et UCPA Formation depuis 2004
+
+          - Auteur d'articles sportifs et biomécaniques
+
+          - Auteur de :
+
+          Repenser l'équitation, 2021, Éditions DésIris (à paraître). Ouvrage co-écrit avec Joséphine Lyon.
+          Corriger le pied sans semelle - nouvelle édition, 2019, Éditions DésIris
+          Corriger la posture et les instabilités articulaires, 2019, Éditions DésIris
+          Guide de la foulée- nouvelle édition, 2016, Éditions DésIris
+          Améliorer sa posture du quotidien à la pratique sportive, 2016, Éditions DésIris
+          Gestuelle Dynamique du mouvement, 2006, Éditions Jouvence
+          <br>
+          <a href="http://www.eadconcept.com">Site : http://www.eadconcept.com</a>
         </p>
-
       </div>
-
     </div>
-
+    <div class="video-box">
+      <div class="video-box video-box--modal video-box--modal--1">
+        <div>
+          <h4>Video 1</h4>
+        </div>
+        <div class="video--modal ">
+          <video class="video--modal video--modal--1" src="../../../public/video/video-1.mp4" autoplay ></video>
+        </div>
+      </div>
+      <div class="video-box video-box--modal video-box--modal--2">
+        <div>
+          <h4>Video 2</h4>
+        </div>
+        <div class="video--modal video--modal--2">
+        </div>
+      </div>
+      <div class="video-box video-box--modal video-box--modal--3">
+        <div>
+          <h4>Video 3</h4>
+        </div>
+        <div class="video--modal video--modal--3">
+        </div>
+      </div>
+      <div class="video-box video-box--modal video-box--modal--4">
+        <div>
+          <h4>Video 4</h4>
+        </div>
+        <div class="video--modal video--modal--4">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -135,6 +166,17 @@ export default {
           'transform': 'scale(' + 0.5 + ')',
           'transition': 'all ' + 1.5 + 's'
         });
+      }
+      if(this.scrollpx > 1200){
+        $(".author__text").css({
+          'transform': 'translate(' + 0+','+ 0 + ')',
+          'transition': 'all ' + 1.5 + 's'
+        })
+        $(".profile-picture--box").css({
+          'transform': 'translate(' + 0 + ')',
+          'opacity': 1,
+          'transition': 'all ' + 1.5 + 's'
+        })
       }
     }
   },
